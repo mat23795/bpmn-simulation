1f025475500c50ea39c9631277beaacbdf5331bd
mat23795 <mat23795@hotmail.com>
2019-03-28 15:25:07 +0100

    first commit

---
+++ app/index.js
@@ -0,0 +1,84 @@
+
+
+var container = $('#js-drop-zone');
+
+
+var viewer = new BpmnJS({
+    container: $('#js-canvas'),
+    height: 600
+});
+
+
+function openDiagram(xml) {
+
+    viewer.importXML(xml, function(err) {
+
+        if (err) {
+            container
+                .removeClass('with-diagram')
+                .addClass('with-error');
+
+            container.find('.error pre').text(err.message);
+
+            console.error(err);
+        } else {
+            container
+                .removeClass('with-error')
+                .addClass('with-diagram');
+        }
+        viewer.get('canvas').zoom('fit-viewport');
+
+
+    });
+}
+
+function registerFileDrop(container, callback) {
+
+    function handleFileSelect(e) {
+        e.stopPropagation();
+        e.preventDefault();
+
+        var files = e.dataTransfer.files;
+
+        var file = files[0];
+
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+
+            var xml = e.target.result;
+
+            callback(xml);
+        };
+
+        reader.readAsText(file);
+    }
+
+    function handleDragOver(e) {
+        e.stopPropagation();
+        e.preventDefault();
+
+        e.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+    }
+
+    container.get(0).addEventListener('dragover', handleDragOver, false);
+    container.get(0).addEventListener('drop', handleFileSelect, false);
+}
+
+$('#js-drop-zone')
+    .on('dragover dragenter', function() {
+        $('#js-drop-zone').css('background-color', 'AliceBlue');
+    })
+    .on('dragleave dragend drop', function() {
+        $('#js-drop-zone').css('background-color', 'white');
+    })
+
+// check file api availability
+if (!window.FileList || !window.FileReader) {
+    window.alert(
+        'Looks like you use an older browser that does not support drag and drop. ' +
+        'Try using Chrome, Firefox or the Internet Explorer > 10.');
+} else {
+    registerFileDrop(container, openDiagram);
+}
+
